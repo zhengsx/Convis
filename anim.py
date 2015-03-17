6d5b065e25939a9565cfe433e987be940432beff
shuxinzheng <zhengsx@mail.ustc.edu.cn>
2015-03-18 02:17:36 +0800

    add a point animation with line

--- anim.py
+++ anim.py
@@ -5,3 +5,8 @@ def update_lines(num, dataLines, lines) :
     lines.set_data(dataLines[0:2,: num])
     lines.set_3d_properties(dataLines[2, :num])
     return lines
+
+def update_points(num, Points, point):
+    point.set_data(Points[0, num], Points[1, num])
+    point.set_3d_properties(Points[2, num])
+    return point
--- main.py
+++ main.py
@@ -12,10 +12,10 @@ ax = axes3d.Axes3D(fig)
 surface_interval = 0.25
 functype = func.functype("GoldsteinPrice")
 F = functype.func(surface_interval)
-ax.plot_surface(F[0], F[1], F[2], rstride=1, cstride=1, cmap='coolwarm', linewidth=0, antialiased=False, norm=matplotlib.colors.LogNorm())
+ax.plot_surface(F[0], F[1], F[2], rstride=1, cstride=1, cmap='coolwarm', linewidth=0, alpha = 0.5, antialiased=False, norm=matplotlib.colors.LogNorm())
 initPoint = (-0.8, 1.7)
 epoch = 500
-#
+
 # GDlines = opt.GradientDescent(initPoint, functype,learningrate=0.000001, epoch=epoch)
 # # print(GDline)
 # gdline = ax.plot(GDlines[0, 0:1], GDlines[1, 0:1], GDlines[2, 0:1], linewidth= 2, color = 'green')[0]
@@ -30,11 +30,9 @@ epoch = 500
 # agline_ani = ani.FuncAnimation(fig, anim.update_lines, epoch, fargs=(AGlines, agline), interval=1, blit=False)
 
 ADlines = opt.AdaDelta(initPoint, functype, rho = 0.95, e = 0.01, epoch=epoch)
-# print(ADline)
+# print(ADlines)
 adline = ax.plot(ADlines[0, 0:1], ADlines[1, 0:1], ADlines[2, 0:1], linewidth= 2, color = 'blue')[0]
 adline_ani = ani.FuncAnimation(fig, anim.update_lines, epoch, fargs=(ADlines, adline), interval=1, blit=False)
-
+adpoint = ax.plot(ADlines[0, 0:1], ADlines[1,0:1], ADlines[2,0:1],linestyle='', marker='o', color='b')[0]
+adpoint_ani = ani.FuncAnimation(fig, anim.update_points, epoch, fargs=(ADlines, adpoint), interval=1,blit=False)
 plb.show()
-
-
-
